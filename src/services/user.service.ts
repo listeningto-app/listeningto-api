3d332cd0faf91893c7a4c47500916c0582028e55
Farko01 <renanricci01@gmail.com>
2022-05-19 08:55:56 -0300

    Alterada porta para 8080

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -32,6 +32,7 @@ async function _read(id: string): Promise<IUser> {
   return userDoc.toObject();
 }
 
+
 // Operação UPDATE
 async function _update(id: string, newData: IUser): Promise<IUser> {
   let userDoc: mongoose.Document & IUser = await dbs.getDocumentById("UserModel", id);
